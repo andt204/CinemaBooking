2eece1ab08a07b17e17a9b5d28749115dec48cf9
NCM03 <cuongnmhe172187@fpt.edu.vn>
2024-10-06 01:24:48 +0700

    PushLoginUI

---
+++ CinemaBooking/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
