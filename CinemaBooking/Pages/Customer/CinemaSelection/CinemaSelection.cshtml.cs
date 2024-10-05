677c91de992720b5277d45e3d3f391f113e59e06
danh mat em <145086417+dotruongan204@users.noreply.github.com>
2024-10-05 23:58:20 +0700

    feature/add-some-page-main-flow

---
+++ CinemaBooking/Pages/Customer/CinemaSelection/CinemaSelection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking.Pages.Customer.Booking
+{
+    public class CinemaSelectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
