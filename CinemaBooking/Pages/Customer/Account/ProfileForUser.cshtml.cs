547704c85db95bce5514dcb26a83119c4b49eb3d
NCM03 <cuongnmhe172187@fpt.edu.vn>
2024-10-06 15:37:56 +0700

    Add Profile DetailFilm

---
+++ CinemaBooking/Pages/Customer/Account/ProfileForUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking.Pages.Customer.Account
+{
+    public class ProfileForUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
