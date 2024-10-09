3707d1931a408e9133e259daa3d7ca8a712a5749
toanpm0510 <thanhyasua1@gmail.com>
2024-10-09 09:24:39 +0700

    feature/add-detail-history-ticket-customer

---
+++ CinemaBooking/Pages/Customer/Ticket/DetailTicket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking.Pages.Customer.Ticket
+{
+    public class DetailTicketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CinemaBooking/Pages/Customer/Ticket/HistoryTicket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaBooking.Pages.Customer.Ticket
+{
+    public class HistoryTicketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
