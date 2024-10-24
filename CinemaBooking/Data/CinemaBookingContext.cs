777be437dcc371a63d853d45d309ad526febb7fa
Do Truong An <145086417+dotruongan204@users.noreply.github.com>
2024-10-25 03:23:55 +0700

    feature/seat-selection

--- CinemaBooking/Data/CinemaBookingContext.cs
+++ CinemaBooking/Data/CinemaBookingContext.cs
@@ -40,10 +40,11 @@ namespace CinemaBooking.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string connectStr = config.GetConnectionString("CinemaBooking");
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =localhost; database =CinemaBooking;uid=sa;pwd=123;TrustServerCertificate=true;");
+                optionsBuilder.UseSqlServer(connectStr);
             }
         }
 
