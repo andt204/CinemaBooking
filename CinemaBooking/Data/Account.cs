bd443dcefed5d8c6a8fbb74762a4ed90291ca1c2
Do Truong An <145086417+dotruongan204@users.noreply.github.com>
2024-11-11 08:40:11 +0700

    fix/comment

--- CinemaBooking/Data/Account.cs
+++ CinemaBooking/Data/Account.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CinemaBooking.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CinemaBooking.Data
 {
@@ -14,12 +16,28 @@ namespace CinemaBooking.Data
         }
 
         public int AccountId { get; set; }
+        [Required(ErrorMessage = "Customer Name is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "The length of name is from 3 to 20 characters")]
+        [Display(Name = "Customer Name")]
         public string FullName { get; set; } = null!;
+
         public string? Avatar { get; set; }
+        [Required(ErrorMessage = "Customer Gender is required")]
+        [Display(Name = "Customer Gender")]
         public string Gender { get; set; } = null!;
+        [CustomValidation(typeof(PhoneValitdation), nameof(PhoneValitdation.ValidatePhone))]
+        [Display(Name = "Customer Phone")]
+
         public string? PhoneNumber { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [Display(Name = "Date Of birth")]
+        [CustomValidation(typeof(AgeValidation), nameof(AgeValidation.ValidateAge))]
+        public DateTime? DateOfBirth { get; set; }
+        [CustomValidation(typeof(EmailValidation), nameof(EmailValidation.ValidateEmail))]
+        [Display(Name = "Email")]
         public string Email { get; set; } = null!;
+
+        [CustomValidation(typeof(PasswordValidation), nameof(PasswordValidation.ValidatePassword))]
         public string Password { get; set; } = null!;
         public byte Status { get; set; }
         public int RoleId { get; set; }
